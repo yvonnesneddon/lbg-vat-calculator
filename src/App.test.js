a4203cd7a21a2afa4471a533ddf04278fcd978f2
Yvonne Sneddon <Yvonne Sneddon>
2024-09-11 15:04:27 +0100

    deleted lines

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 test('renders VAT Calculator text', () => {
   render(<App />);
   const heading = screen.getByText(/VAT CALCULATOR/i);
